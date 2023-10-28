afb65b0c6cd1d9df7129b16ed4c777a71fb07044
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-10-28 15:01:56 +0100

    working with add-product page

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,72 @@
+import { prisma } from "@/libs/db/prisma"
+import { redirect } from "next/navigation"
+
+export const metadata = {
+    title: 'Add Product'
+}
+
+async function addProduct(formData: FormData){
+    'use server'
+
+    const name = formData.get('name')?.toString()
+    const description = formData.get('description')?.toString()
+    const price = Number(formData.get('price') || 0)
+    const imageUrl = formData.get('imageUrl')?.toString()
+
+    if(!name || !description || !imageUrl || !price){
+        throw Error('Missing required fields')
+    }
+
+    await prisma.product.create({
+        data: {
+            name,
+            description,
+            imageUrl, 
+            price
+        }
+    })
+
+    redirect("/")
+}
+
+export default function AddProductPage() {
+    
+
+    return (
+        <div>
+            <h1 className='text-lg mb-3 font-bold'>Add Product Page</h1>
+
+            <form action={addProduct}>
+                <input
+                    required
+                    name='name'
+                    placeholder='Name'
+                    type="text"
+                    className='mb-3 w-full input input-bordered'
+                />
+                <textarea
+                    required
+                    name='description'
+                    placeholder='Description'
+                    className='textarea textarea-bordered mb-3 w-full'
+                ></textarea>
+                <input
+                    required
+                    name='imageUrl'
+                    placeholder='Image URL'
+                    type="url"
+                    className='mb-3 w-full input input-bordered'
+                />
+                <input
+                    required
+                    name='price'
+                    placeholder='Price'
+                    type="number"
+                    className='mb-3 w-full input input-bordered'
+                />
+
+                <button type="submit" className='btn btn-primary btn-block'>Add Product</button>
+            </form>
+        </div>
+    )
+}
