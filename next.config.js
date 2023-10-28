d75e8581965893d6364e146efd5795f4e5cd6da2
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-10-28 14:31:26 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {hostname: "images.unsplash.com"}
+        ]
+    },
+    experimental: {
+        serverActions: true
+    }
+}
 
 module.exports = nextConfig
